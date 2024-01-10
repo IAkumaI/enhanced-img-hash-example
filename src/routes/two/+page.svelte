a080f0ef0fda7560ca449550d247bc4022fb6f12
Akuma <meru.no.akuma@gmail.com>
2024-01-10 16:41:20 +0300

    Init

---
+++ src/routes/two/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Page Two</h1>
+<h2>{'<enhanced:img src="./image.png" />'}</h2>
+<enhanced:img src="./image.png" />
+<h2>Real image:</h2>
+<enhanced:img src="./image.png?456" />
+<ul>
+    <li><a href="/one">Go to One</a></li>
+</ul>
