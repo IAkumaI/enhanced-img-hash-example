10049b852eceeebf4e588b5437430f0e260d60e4
Akuma <meru.no.akuma@gmail.com>
2024-01-10 16:45:46 +0300

    Better headers

--- src/routes/one/+page.svelte
+++ src/routes/one/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Page One</h1>
 <h2>{'<enhanced:img src="./image.png" />'}</h2>
 <enhanced:img src="./image.png" />
-<h2>Real image:</h2>
+<h2>{'<enhanced:img src="./image.png?123" />'}</h2>
 <enhanced:img src="./image.png?123" />
 <ul>
     <li><a href="/two">Go to Two</a></li>
--- src/routes/two/+page.svelte
+++ src/routes/two/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Page Two</h1>
 <h2>{'<enhanced:img src="./image.png" />'}</h2>
 <enhanced:img src="./image.png" />
-<h2>Real image:</h2>
+<h2>{'<enhanced:img src="./image.png?456" />'}</h2>
 <enhanced:img src="./image.png?456" />
 <ul>
     <li><a href="/one">Go to One</a></li>
